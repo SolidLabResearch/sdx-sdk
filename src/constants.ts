2d7c98b53246d16883b18c35a7ea74b6d46fba32
Thomas Dupont <thomas.dupont@ugent.be>
2023-03-22 11:30:53 +0100

    constants refactoring

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+// Generate paths
+export const URI_SDX_GENERATE_FOLDER = `/.sdx-gen`;
+export const URI_SDX_GENERATE_SHACL_FOLDER = `${URI_SDX_GENERATE_FOLDER}/shacl`;
+export const URI_SDX_GENERATE_GRAPHQL_FOLDER = `${URI_SDX_GENERATE_FOLDER}/graphql`;
+export const URI_SDX_GENERATE_GRAPHQL_SCHEMA = `${URI_SDX_GENERATE_GRAPHQL_FOLDER}/schema.graphqls`;
--- src/sdx-client.ts
+++ src/sdx-client.ts
@@ -1,4 +1,5 @@
 import { DocumentNode, ExecutionResult, graphql, print } from "graphql";
+import { URI_SDX_GENERATE_SHACL_FOLDER } from "./constants.js";
 // import { TEST_COMPLEX_SHACL_FILE_PATH, TEST_SHACL_FILE_PATH } from "../constants.js";
 // import { ShaclParserService } from "../services/shacl-parser.service.js";
 import * as legacy from "./legacy-sdx-client.js";
@@ -10,9 +11,9 @@ export function legacyRequester(podLocation: string) {
     return async <R, V>(doc: DocumentNode, vars?: V, options?: {}): Promise<ExecutionResult<R>> => {
         const query = print(doc);
         if (!parser.primed) {
-            await parser.primeCache('.sdx/shacl');
+            await parser.primeCache(URI_SDX_GENERATE_SHACL_FOLDER);
         }
-        const schema = await parser.parseSHACLs('.sdx/shacl');
+        const schema = await parser.parseSHACLs(URI_SDX_GENERATE_SHACL_FOLDER);
 
         const result = await graphql({
             source: query,
