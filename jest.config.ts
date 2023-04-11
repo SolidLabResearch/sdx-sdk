704ff445adef98b757c8172781e5b28f9b02aa22
tdupont <thomas.dupont@ugent.be>
2023-04-11 12:44:34 +0200

    test: jest-extended config

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,12 @@ import type { JestConfigWithTsJest } from 'ts-jest/dist/types';
 const config: JestConfigWithTsJest = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    moduleDirectories: ["node_modules", "src"]
+    moduleDirectories: ["node_modules", "src"],
+    testPathIgnorePatterns: ["__tests__/assets/", "globals.d.ts"],
+    transform: {
+        '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.jest.json' }],
+    },
+    setupFilesAfterEnv: ["jest-extended/all"],
 };
 
 export default config;
